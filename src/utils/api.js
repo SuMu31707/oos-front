594bebf7b749ffc080a92c3c691c9f01f4e73774
SuMu <SuMu31707@163.com>
2021-03-01 20:24:51 +0800

    测试登录返回Token

--- src/utils/api.js
+++ src/utils/api.js
@@ -54,3 +54,30 @@ export const postRequest = (url,params) =>{
 		data: params
 	})
 }
+
+// 传送json格式的put请求
+export const putRequest = (url,params) =>{
+	return axios({
+		method: 'put',
+		url: `${base}${url}`,
+		data: params
+	})
+}
+
+// 传送json格式的get请求
+export const getRequest = (url,params) =>{
+	return axios({
+		method: 'get',
+		url: `${base}${url}`,
+		data: params
+	})
+}
+
+// 传送json格式的delete请求
+export const deleteRequest = (url,params) =>{
+	return axios({
+		method: 'delete',
+		url: `${base}${url}`,
+		data: params
+	})
+}
