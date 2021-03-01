9531c3004eff02f4b328dc9b6f5ee5d329cd2169
SuMu <SuMu31707@163.com>
2021-03-01 22:00:35 +0800

    安装Vuex

--- src/main.js
+++ src/main.js
@@ -1,9 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import store from './store'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
-import axios from 'axios'
 import {deleteRequest, getRequest, postRequest, putRequest} from "@/utils/api";
 
 
@@ -19,5 +19,6 @@ Vue.prototype.deleteRequest = deleteRequest;
 
 new Vue({
   router,
+  store,
   render: h => h(App)
 }).$mount('#app')
