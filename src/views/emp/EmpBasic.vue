e71825ed7ae53a764c793387514890be07bcfe2d
SuMu <SuMu31707@163.com>
2021-03-07 21:23:18 +0800

    更新操作员角色的实现

--- src/views/emp/EmpBasic.vue
+++ src/views/emp/EmpBasic.vue
@@ -1,5 +1,23 @@
 <template>
-  <div>基本资料</div>
+  <div>
+    <div style="display: flex;justify-content: space-between">
+      <div>
+        <el-input type="text" style="width: 300px;margin-right: 5px" placeholder="请输入员工姓名进行搜索..." prefix-icon="el-icon-search"></el-input>
+        <el-button type="primary" icon="el-icon-search">搜索</el-button>
+        <el-button type="primary" icon="el-icon-search">
+          <i class="fa fa-angle-double-down" aria-hidden="true"></i>
+          高级搜索</el-button>
+      </div>
+      <div>
+        <el-button type="success"><i class="fa fa-level-down" aria-hidden="true"></i>导入数据</el-button>
+        <el-button type="success"><i class="fa fa-level-up" aria-hidden="true"></i>导出数据</el-button>
+        <el-button type="primary" icon="el-icon-plus">添加员工</el-button>
+      </div>
+    </div>
+    <div>
+
+    </div>
+  </div>
 </template>
 
 <script>
